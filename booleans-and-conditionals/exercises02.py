ab914552794cd316afc07c451dbc0371e03db2b2
dreadpiratehera <zink.joshua@gmail.com>
2025-11-04 10:29:39 -0600

    python

--- booleans-and-conditionals/exercises02.py
+++ booleans-and-conditionals/exercises02.py
@@ -9,13 +9,33 @@ shuttle_speed = 15000
 
 # a) If crew_status is true, print "Crew Ready" else print "Crew Not Ready".
 
+if crew_status == True:
+    print("Crew ready")
+else:
+    print("Crew not ready")
+
 
 # b) If computer_status_code is 200, print "Please stand by. Computer is rebooting." Else if computer_status_code is 400, print "Success! Computer online." Else print "ALERT: Computer offline!"
 
+if computer_status_code == 200:
+    print("Please stand by. Computer is rebooting.")
+elif computer_status_code == 400:
+    print("Success! Computer online.")
+else:
+    print("ALERT: computer offline!")
+
 
 # c) If shuttle_speed is > 17,500, print "ALERT: Escape velocity reached!" Else if shuttle_speed is < 8000, print "ALERT: Cannot maintain orbit!" Else print "Stable speed".
+
+if shuttle_speed > 17500:
+    print("ALERT: Escape velocity reached!")
+elif shuttle_speed < 8000:
+    print("ALERT: Cannot maintain orbit!")
+else:
+    print("Stable speed.")
 
 
 # 4) PREDICT: Do the code blocks shown in the Section D produce the same result?
 
 # print("Yes" or "No")
+print("Yes")
