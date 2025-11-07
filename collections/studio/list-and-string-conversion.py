e1be2ae2847ebae5618830da600b157eed6a104e
dreadpiratehera <zink.joshua@gmail.com>
2025-11-06 21:23:37 -0600

    part1

--- collections/studio/list-and-string-conversion.py
+++ collections/studio/list-and-string-conversion.py
@@ -5,19 +5,61 @@ proto_list4 = "Comma-spaces, might, require, typing, caution"
 
 strings = [proto_list1, proto_list2, proto_list3, proto_list4]
 
-# a) Use the 'in' method to check to see if the words in each string are separated by commas (,), semicolons (;) or just spaces.
+# a) Use the 'in' method to check to see if the words in each string are separated by commas (,), 
+# semicolons (;) or just spaces.
+
+protoProtoList = [proto_list1, proto_list2, proto_list3, proto_list4]
+CommaCount = 0
+SemicolonCount = 0
+SpaceCount = 0
+CommaSpaceCount = 0
+for comma in protoProtoList:
+    CommaCount += 1
+    if ',' in comma:
+        print("The characters are separated by commas in proto_list" + str(CommaCount))
+for semicolon in protoProtoList:
+    SemicolonCount += 1
+    if ';' in semicolon:
+        print("THe characters are separated by semicolons in proto_list" + str(SemicolonCount))
+for spaces in protoProtoList:
+    SpaceCount += 1
+    if ' ' in spaces:
+        print("THe characters are separated by spaces in proto_list" + str(SpaceCount))
 
 
-# b) If the string uses commas to separate the words, split it into an array, reverse the entries, and then join the array into a new comma separated string.
+# b) If the string uses commas to separate the words, split it into an array, reverse the entries, 
+# and then join the array into a new comma separated string.
+
+CommaArray = proto_list1.split(',')
+CommaArray.reverse()
+CommaString = ','.join(CommaArray)
+print(CommaString)
+
+
+# c) If the string uses semicolons to separate the words, split it into an array, alphabetize the 
+# entries, and then join the array into a new comma separated string.
+
+SemicolonArray = proto_list2.split(';')
+SemicolonArray.sort()
+SemicolonString = ";".join(SemicolonArray)
+print(SemicolonString)
 
 
 
-# c) If the string uses semicolons to separate the words, split it into an array, alphabetize the entries, and then join the array into a new comma separated string.
+# d) If the string uses spaces to separate the words, split it into an array, reverse alphabetize 
+# the entries, and then join the array into a new space separated string.
+
+SpaceArray = proto_list3.split(' ')
+SpaceArray.sort(reverse=True)
+SpaceString = " ".join(SpaceArray)
+print(SpaceString)
 
 
 
-# d) If the string uses spaces to separate the words, split it into an array, reverse alphabetize the entries, and then join the array into a new space separated string.
+# e) If the string uses ‘comma spaces’ to separate the list, modify your code to produce the 
+# same result as part “b”, making sure that the extra spaces are NOT part of the final string.
 
-
-
-# e) If the string uses ‘comma spaces’ to separate the list, modify your code to produce the same result as part “b”, making sure that the extra spaces are NOT part of the final string.
+CommaSpaceArray = proto_list4.split(', ')
+CommaSpaceArray.reverse()
+CommaSpaceString = ",".join(CommaSpaceArray)
+print(CommaSpaceString)
--- collections/studio/string-modification.py
+++ collections/studio/string-modification.py
@@ -3,13 +3,48 @@ my_string = "LaunchCode"
 
 # a) Use string methods to remove the first three characters from the string and add them to the end.
 
-
+beg_string = my_string[3:]
+end_string = my_string[:3]
+newstring = beg_string + end_string
+print (newstring)
 
 # Use a template literal to print the original and modified string in a descriptive phrase.
+
+print(f"The original string is {my_string}, the modified string is {newstring}")
 
 
 
 # b) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
 
-# c) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+mod = 0
+while mod == 0:
+    mod = int(input("How many characters do you want to move to the end?"))
+
+beg_string = my_string[(mod):]
+end_string = my_string[:(mod)]
+newstring = beg_string + end_string
+
+print(f"The original string is {my_string}, the modified string is {newstring}")
+
+
+# c) Add validation to your code to deal with user inputs that are longer than the word. 
+# In such cases, default to moving 3 characters. Also, the template literal should note the error.
+
+mod = 0
+while mod == 0:
+    mod = int(input("How many characters do you want to move to the end?"))
+
+if mod >10:
+    beg_string = my_string[3:]
+    end_string = my_string[:3]
+    newstring = beg_string + end_string
+    print(f"Too many character selected. Default of 3 chosen insead.")
+
+
+else:
+    beg_string = my_string[(mod):]
+    end_string = my_string[:(mod)]
+    newstring = beg_string + end_string
+
+print(f"The original string is {my_string}, the modified string is {newstring}")
